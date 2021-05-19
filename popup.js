649fb528bb30766824a587ccbe6bb34598ecb561
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-05-19 10:09:28 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,5 +1,23 @@
 var checksite = {
   urlhaus:function(domain){
+    return new Promise(res => {
+      try{
+        fetch('https://raw.githubusercontent.com/curbengh/urlhaus-filter/master/urlhaus-filter-domains.txt').then(async function(req){
+          var text = await req.text()
+          var split = text.split("\n")
+          for(var t = 0;t < split.length;t++){
+            if(split[t].startsWith("#")){continue}
+            if(split[t] === domain){
+              res(true)
+            }
+          }
+          res(false)
+        }).catch(function(){res(null)})
+      }
+      catch(err){
+        res(null)
+      }
+    })
   },
   gethostorurl:function gethostorurl(url){
     try{
@@ -22,6 +40,10 @@ var main = async function(){
   var url = await checksite.getTabURL()
   console.log(checksite.gethostorurl(url),url)
 document.getElementById('hostname').textContent = checksite.gethostorurl(await checksite.getTabURL())
+  var result_urlhaus = await checksite.urlhaus(checksite.gethostorurl(url))
+  document.getElementById('urlhaus').textContent = (result_urlhaus === true)?"malware":(result_urlhaus === false)?"safe":"unknown"
   
 }
-main().catch(console.log)
+main().catch(function(err){
+  console.trace("Error:",err)
+})
