f5192c831f15b66275797421d5bc951d85b90f27
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-05-19 09:49:23 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,22 @@
 var checksite = {
   urlhaus:function(domain){
+  },
+  gethostorurl:function gethostorurl(url){
+    try{
+      return (new URL(url).hostname||url)
+    }
+    catch(err){
+      return url
+    }
+  },
+  getTabURL: function(){
+    return new Promise(res => {
+      chrome.tabs.query({active:true,currentWindow:true},function(tab){
+        res(tab.url)
+      })
+    })
   }
 }
+(async function(){
+document.getElementById('hostname').textContent = checksite.gethostorurl(await getTabURL())
+})()
