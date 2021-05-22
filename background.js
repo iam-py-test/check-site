b0d1321aee08dcf56ed55deeea77569a3c535f0e
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-05-22 17:22:29 -0400

    Update background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-//chrome.contextMenus.create({title:"Check link",id:"linkcheck",contexts:["link"]})
-//chrome.contextMenus.onClicked.addListener(function(data,tab){
-//  console.log(data,tab,1)
-//})
+chrome.contextMenus.create({title:"Check link",id:"linkcheck",contexts:["link"]})
+chrome.contextMenus.onClicked.addListener(function(data,tab){
+  console.log(data,tab,1)
+})
