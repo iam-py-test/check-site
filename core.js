803f9433c6df9c8823a4c8416a8b6f721d7d92f3
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-05-24 15:54:43 -0400

    Update core.js

--- core.js
+++ core.js
@@ -126,3 +126,23 @@ window.checksite = {
     })
   }
 }
+
+
+var hostslists = {
+  lists:new Map([["The BlockList Project fraud","https://raw.githubusercontent.com/blocklistproject/Lists/master/fraud.txt"],["The BlockList Project malware","https://raw.githubusercontent.com/blocklistproject/Lists/master/malware.txt"]]),
+  loadHOSTS:function(domain,list){
+    return new Promise(res => {
+      fetch(list).then(async function(req){
+        var text = await req.text()
+        var stext = text.split("\n")
+        for(var t = 0;t < stext.length;t++){
+          if(stext[t].startsWith('#')){continue}
+          if(domain === stext[t].split(" ")[1]){
+            res(true)
+          }
+        }
+        res(false)
+      }).catch(function(){res(null)})
+    })
+  }
+}
