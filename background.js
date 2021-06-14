fc44246fb9801e576cfcf178c87b4df31882f8f6
iam-py-test <84232764+iam-py-test@users.noreply.github.com>
2021-06-14 14:33:08 -0400

    Update background.js

--- background.js
+++ background.js
@@ -4,9 +4,18 @@ var htmlencode = function(data){
 
 chrome.contextMenus.create({title:"Check link",id:"linkcheck",contexts:["link"]})
 chrome.contextMenus.onClicked.addListener(async function(data,tab){
+  
   console.log(data,tab,1)
+  if(data.menuItemId === "linkcheck"){
 chrome.windows.create({type:"popup","url":"link_report.html?url=" + encodeURIComponent(data.linkUrl)})
+  }
+  if(data.menuItemId === 'forceu'){
+    checksite.private.updateLists()
+  }
 })
+
+chrome.contextMenus.create({"title":"Force an cache update",id:"forceu",contexts:["browser_action"]})
+
 try{
 checksite.private.updateLists()
 }
